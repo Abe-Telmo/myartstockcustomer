34269ef8b5b1849d7f3bec1050d4d8cd63afd2be
Olasunkanmi <alabiolasunkanmi@outlook.com>
2021-05-02 19:59:11 +0100

    fix

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const sideBar = document.querySelector('.side-bar')
-
-document.querySelector('.toggle').onclick = function () {
-  this.classList.toggle('active')
-  sideBar.classList.toggle('active')
-}
